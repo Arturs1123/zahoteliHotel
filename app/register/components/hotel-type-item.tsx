1703841cc6e4a70c9bfde636cb6bc7b029f003a5
lovemonday-dev <bluespider0102@gmail.com>
2024-08-23 16:02:03 +0900

    update image src in hotel-type-item

--- app/register/components/hotel-type-item.tsx
+++ app/register/components/hotel-type-item.tsx
@@ -4,7 +4,7 @@ export default function HotelTypeItem({ thumb = '', label = '', selected = false
     return (
         <div className={`${selected ? 'border border-accent' : ''} rounded-[16px] p-[24px] shadow-md cursor-pointer`} onClick={() => onSelect(label)}>
             <div className="lg:h-[200px] h-[245px] mb-[16px]">
-                <img src={thumb} alt={label} className="w-full h-full object-cover rounded-[8px]" />
+                <img src={process.env.NEXT_PUBLIC_BACKEND_API_URL + thumb} alt={label} className="w-full h-full object-cover rounded-[8px]" />
             </div>
             <div className="flex justify-between items-center">
                 <span className="text-h6">{label}</span>
